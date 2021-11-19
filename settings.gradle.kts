6df5a282b4c2427bc3d62d8d118907198345c502
ts-xueyuan.ma <ts-xueyuan.ma@rakuten.com>
2021-11-19 18:21:52 +0900

    origin commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GithubFollowersKMM"
+include(":GithubFollowersAndroid")
+include(":shared")
